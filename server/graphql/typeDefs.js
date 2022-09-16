0e331a6fc8aa5edaafe2e4784ca2832c8c19730e
Anjali Shah <anjushah2507@gmail.com>
2022-09-16 17:32:53 +0530

    added comments and like post

--- server/graphql/typeDefs.js
+++ server/graphql/typeDefs.js
@@ -6,7 +6,22 @@ module.exports = gql`
     body: String!
     createdAt: String!
     username: String!
+    comments:[Comment]
+    likes:[Like]
   }
+  type Comment{
+    id:ID!
+    username:String!
+    body:String!
+    createdAt:String!
+  }
+
+  type Like{
+    id:ID!
+    createdAt:String!
+    username:String!
+  }
+
   type User {
     id: ID!
     email: String!
@@ -29,5 +44,12 @@ module.exports = gql`
     login(username: String!, password: String!): User!
     createPost(body:String!):Post!
     deletePost(postId:ID!):String!
+    createComment(postId:ID!, body:String!):Post!
+    deleteComment(postId:ID!,commentId:ID!):Post!
+    likePost(postId:ID!):Post!
+  }
+
+  type Subscription{
+    newPost: Post!
   }
 `;
--- server/index.js
+++ server/index.js
@@ -1,21 +1,23 @@
-const { ApolloServer } = require('apollo-server');
-const mongoose = require('mongoose');
+const { ApolloServer } = require("apollo-server");
+const { PubSub } = require("graphql-subscriptions");
+const mongoose = require("mongoose");
 
-const typeDefs = require('./graphql/typeDefs');
-const resolvers = require('./graphql/resolvers');
-const { MONGODB } = require('./config.js');
+const typeDefs = require("./graphql/typeDefs");
+const resolvers = require("./graphql/resolvers");
+const { MONGODB } = require("./config.js");
 
+const pubsub = new PubSub();
 const server = new ApolloServer({
   typeDefs,
   resolvers,
-  context:({req})=>({req})
+  context: ({ req }) => ({ req, pubsub }),
 });
 
 mongoose
   .connect(MONGODB, { useNewUrlParser: true })
   .then(() => {
-    console.log('MongoDB Connected');
-    return server.listen({ port: 5000 });
+    console.log("MongoDB Connected");
+    return server.listen({ port: 5001 });
   })
   .then((res) => {
     console.log(`Server running at ${res.url}`);
