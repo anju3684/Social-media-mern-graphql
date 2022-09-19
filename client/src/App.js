bfb981768de9bf4804ab4bc0ecda8f4016aed1d4
Anjali Shah <anjushah2507@gmail.com>
2022-09-19 18:15:47 +0530

    added routes and setting up semantic ui

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
-import React from "react"
-function App() {
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Container } from 'semantic-ui-react';
+import './App.css';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+// import MenuBar from './components/MenuBar';
+// import SinglePost from './pages/SinglePost';
+const App = () => {
   return (
-    <div>
-      <h1>Hello world!</h1>
-    </div>
+    <BrowserRouter>
+      <Container>
+        {/* <MenuBar /> */}
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact path='/login' element={<Login />} />
+          <Route exact path='/register' element={<Register />} />
+          {/* <Route exact path='/posts/:postId' element={<SinglePost />} /> */}
+        </Routes>
+      </Container>
+    </BrowserRouter>
   );
-}
-
+};
 export default App;
